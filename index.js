68d0d3035f579eff2d5f064d2a254f292bac52e3
Liu Chao <lc@smartkarma.com>
2018-08-26 18:57:26 +0800

    add more start files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ node
+#!/usr/bin/env node
 
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -16,49 +16,51 @@ const QUESTIONS = [
     name: 'project-name',
     type: 'input',
     message: 'Project name:',
-    validate: function (input) {
+    validate: function(input) {
       if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
+      else
+        return 'Project name may only include letters, numbers, underscores and hashes.';
     }
   }
 ];
 
-
 const CURR_DIR = process.cwd();
 
-inquirer.prompt(QUESTIONS)
-  .then(answers => {
-    const projectChoice = answers['project-choice'];
-    const projectName = answers['project-name'];
-    const templatePath = `${__dirname}/templates/${projectChoice}`;
+inquirer.prompt(QUESTIONS).then(answers => {
+  const projectChoice = answers['project-choice'];
+  const projectName = answers['project-name'];
+  const templatePath = `${__dirname}/templates/${projectChoice}`;
 
-    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+  fs.mkdirSync(`${CURR_DIR}/${projectName}`);
 
-    createDirectoryContents(templatePath, projectName);
-  });
+  createDirectoryContents(templatePath, projectName);
+});
 
-function createDirectoryContents (templatePath, newProjectPath) {
+function createDirectoryContents(templatePath, newProjectPath) {
   const filesToCreate = fs.readdirSync(templatePath);
 
   filesToCreate.forEach(file => {
     const origFilePath = `${templatePath}/${file}`;
-    
+
     // get stats about the current file
     const stats = fs.statSync(origFilePath);
 
     if (stats.isFile()) {
       const contents = fs.readFileSync(origFilePath, 'utf8');
 
-        // Rename
+      // Rename
       if (file === '.npmignore') file = '.gitignore';
 
       const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
       fs.writeFileSync(writePath, contents, 'utf8');
     } else if (stats.isDirectory()) {
       fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
-      
+
       // recursive call
-      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+      createDirectoryContents(
+        `${templatePath}/${file}`,
+        `${newProjectPath}/${file}`
+      );
     }
   });
 }
